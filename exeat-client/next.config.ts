6b3d6986068ac73fbe00c6a16f8fccb992ae59c7
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-11-17 20:33:59 +0000

    Update

--- exeat-client/next.config.ts
+++ exeat-client/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This disables ESLint checks during builds
+    ignoreDuringBuilds: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
