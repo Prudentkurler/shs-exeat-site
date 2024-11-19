e75e6bed5592814b7f3620fdbd3db6609538f455
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-11-19 11:17:59 +0000

    Update

--- exeat-client/next.config.ts
+++ exeat-client/next.config.ts
@@ -7,22 +7,8 @@ const nextConfig: NextConfig = {
   },
   // Enabling React Strict Mode
   reactStrictMode: true,
-  // Custom webpack configuration
-  webpack: (config, { isServer }) => {
-    // Modify the config as needed
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
-  // Internationalization settings
-  i18n: {
-    locales: ['en', 'fr', 'es'], // List of languages your site supports
-    defaultLocale: 'en', // Default language
-  },
-  // Page extensions
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-  // Other config options
+
+ 
 };
 
 export default nextConfig;
